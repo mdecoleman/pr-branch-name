467bbd8d04d64b68e62afb1a1c7ea2e3b7edfeed
Matthew Coleman <matthewdecoleman@gmail.com>
2019-12-11 10:40:04 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Pull Request Branch Name
+
+A github action that retrieves the pull request branch name and sets it in the output for other actions to use.
+
+# Usage
+
+```yaml
+- uses: mdecoleman/pr-branch-name@1.0.0
+  id: vars
+  with:
+    repo-token: ${{ secrets.GITHUB_TOKEN }}
+- run: echo ${{ steps.vars.outputs.branch }}
+```
+
+# License
+
+The scripts and documentation in this project are released under the [MIT License](LICENSE)
---
+++ src/main.js
@@ -0,0 +1,40 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+async function run() {
+  try {
+    const token = core.getInput("repo-token");
+    const { owner, repo } = github.context.repo;
+
+    const prNumber = getPrNumber();
+
+    if (!prNumber) {
+      core.setFailed("Could not get pull request number from context");
+    }
+
+    const octokit = new github.GitHub(token);
+
+    const response = await octokit.pulls.get({
+      owner: owner,
+      repo: repo,
+      pull_number: prNumber
+    });
+
+    core.setOutput("branch", response.data.head.ref);
+  } catch (error) {
+    core.error(error);
+    core.setFailed(error.message);
+  }
+}
+
+function getPrNumber() {
+  const pullRequest = github.context.payload.pull_request;
+
+  if (!pullRequest) {
+    return undefined;
+  }
+
+  return pullRequest.number;
+}
+
+run();
