9628b0d637ec7917c41a2f5d99d39ea7c3196a69
Matthew Coleman <matthewdecoleman@gmail.com>
2024-07-20 12:53:23 +0100

    Added: updated build tooling

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,40 @@
-import core from "@actions/core";
-import github from "@actions/github";
+import core from '@actions/core'
+import github from '@actions/github'
 
-async function run() {
+function getPrNumber() {
+  const pullRequest = github.context.payload.pull_request
+
+  if (!pullRequest) {
+    return null
+  }
+
+  return pullRequest.number
+}
+
+export async function run(): Promise<void> {
   try {
-    const token = core.getInput("repo-token");
-    const { owner, repo } = github.context.repo;
+    const token = core.getInput('repo-token')
+    const { owner, repo } = github.context.repo
 
-    const prNumber = getPrNumber();
+    const prNumber = getPrNumber()
 
     if (!prNumber) {
-      core.setFailed("Could not get pull request number from context");
-      return;
+      core.setFailed('Could not get pull request number from context')
+      return
     }
 
-    const octokit = github.getOctokit(token);
+    const octokit = github.getOctokit(token)
 
     const response = await octokit.rest.pulls.get({
       owner: owner,
       repo: repo,
-      pull_number: prNumber,
-    });
+      pull_number: prNumber
+    })
 
-    core.setOutput("branch", response.data.head.ref);
+    core.setOutput('branch', response.data.head.ref)
   } catch (error) {
-    core.setFailed(error.message);
+    if (error instanceof Error) {
+      core.setFailed(error.message)
+    }
   }
 }
-
-function getPrNumber() {
-  const pullRequest = github.context.payload.pull_request;
-
-  if (!pullRequest) {
-    return null;
-  }
-
-  return pullRequest.number;
-}
-
-run();
