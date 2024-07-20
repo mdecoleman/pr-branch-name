d0e02a334363eb22da53bc1ed49a12a0b0634624
Matthew Coleman <matthewdecoleman@gmail.com>
2024-07-20 13:41:59 +0100

    fixed typescipt import issue for imports

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import core from '@actions/core'
-import github from '@actions/github'
+import * as core from '@actions/core'
+import * as github from '@actions/github'
 
 function getPrNumber(): number | null {
   const pullRequest = github.context.payload.pull_request
